e9cb81cb8e2c4ee0850625aba8db39f3c0fb6c73
Caruki <caruki91@posteo.de>
2020-04-10 13:58:53 +0200

    Add styled button component

---
+++ client/src/components/Button.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  width: 136px;
+  height: 46px;
+  border-radius: 20px;
+  background-image: linear-gradient(#504481, #1e194f);
+  color: #d05888;
+  font-size: 17px;
+`;
+
+export default Button;
