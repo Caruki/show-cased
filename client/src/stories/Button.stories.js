dab7cedc7f496123afb11c8a528ccdf43a0f49f6
Caruki <caruki91@posteo.de>
2020-04-10 14:05:19 +0200

    Add button story

---
+++ client/src/stories/Button.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../components/Button';
+
+export default {
+  title: 'Buttons',
+};
+
+export const SignUpButton = () => {
+  return <Button>Sign Up</Button>;
+};
+
+export const LogInButton = () => {
+  return <Button>Log In</Button>;
+};
