a8756cd3b6a90bb73ae6914eda0926b2bd24cb01
Caruki <caruki91@posteo.de>
2020-04-12 17:21:46 +0200

    Add basic side navigation with buttons

---
+++ client/src/components/SideNavigation.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  flex: 0 0 10%;
+  z-index: 10;
+  height: 100%;
+  box-sizing: border-box;
+  position: relative;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  width: 30%;
+  height: 50%;
+  box-sizing: border-box;
+`;
+
+const TextContainer = styled.div`
+  height: fit-content;
+  color: ${(props) => (props.clicked ? '#1E194F' : '#AEB2F5')};
+  font: 300 0.8rem 'Roboto', sans-serif;
+  transform: rotate(-90deg);
+  transform-origin: bottom right;
+`;
+
+const Button = styled.button`
+  height: 50%;
+  width: 100%;
+  background-color: ${(props) => (props.clicked ? '#AEB2F5' : '#504481')};
+  border-radius: 0px 20px 20px 0px;
+  border: hidden;
+  padding: 20px 5px 5px 5px;
+
+  &:focus {
+    outline-width: 0;
+  }
+`;
+
+function SideNavigation({ clicked, site }) {
+  return (
+    <Container>
+      <ButtonContainer>
+        <Button clicked={clicked}>
+          <TextContainer clicked={clicked}>
+            {site === 'popular' ? 'Newest' : 'ToWatch'}
+          </TextContainer>
+        </Button>
+        <Button clicked={clicked}>
+          <TextContainer clicked={clicked}>
+            {site === 'popular' ? 'Trending' : 'Watched'}
+          </TextContainer>
+        </Button>
+      </ButtonContainer>
+    </Container>
+  );
+}
+
+SideNavigation.propTypes = {
+  clicked: PropTypes.bool,
+  site: PropTypes.string,
+};
+
+export default SideNavigation;
--- client/src/stories/Buttons.stories.js
+++ client/src/stories/Buttons.stories.js
@@ -1,14 +1,67 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import SubmitButton from '../components/SubmitButton';
+import SideNavigation from '../components/SideNavigation';
 
 export default {
   title: 'Buttons',
 };
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  background-color: rgba(14, 5, 46, 1);
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  position: relative;
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  flex-grow: 1;
+  overflow: auto;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  position: relative;
+`;
+
 export const SignUpButton = () => {
-  return <SubmitButton>Sign Up</SubmitButton>;
+  return (
+    <AppContainer>
+      <MainContainer>
+        <SubmitButton>Sign Up</SubmitButton>
+      </MainContainer>
+    </AppContainer>
+  );
 };
 
 export const LogInButton = () => {
-  return <SubmitButton>Log In</SubmitButton>;
+  return (
+    <AppContainer>
+      <MainContainer>
+        <SubmitButton>Log In</SubmitButton>
+      </MainContainer>
+    </AppContainer>
+  );
+};
+
+export const SideNavButtonPopular = () => {
+  return (
+    <AppContainer>
+      <SideNavigation site="popular" />
+      <MainContainer />
+    </AppContainer>
+  );
+};
+
+export const SideNavButtonRecs = () => {
+  return (
+    <AppContainer>
+      <SideNavigation site="recs" clicked={true} />
+      <MainContainer />
+    </AppContainer>
+  );
 };
