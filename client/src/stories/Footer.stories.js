14bed2258c43c1d3eb2274a260aa98343de11d69
Caruki <caruki91@posteo.de>
2020-04-12 13:58:04 +0200

    Add App and Main containers to stories to simulate and check bg and flexbox behaviour

--- client/src/stories/Footer.stories.js
+++ client/src/stories/Footer.stories.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import styled from '@emotion/styled';
 
 export default {
   title: 'Footer',
 };
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  background-color: rgba(14, 5, 46, 1);
+  width: 100vw;
+  height: 100vh;
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  flex-grow: 1;
+  overflow: auto;
+  width: 100%;
+  height: 100%;
+`;
+
 export const FooterPopular = () => {
-  return <Footer site="popular" />;
+  return (
+    <AppContainer>
+      <MainContainer />
+      <Footer site="popular" />
+    </AppContainer>
+  );
 };
 
 export const FooterRecs = () => {
-  return <Footer site="recs" />;
+  return (
+    <AppContainer>
+      <MainContainer />
+      <Footer site="recs" />
+    </AppContainer>
+  );
 };
 
 export const FooterLists = () => {
-  return <Footer site="lists" />;
+  return (
+    <AppContainer>
+      <MainContainer />
+      <Footer site="lists" />
+    </AppContainer>
+  );
 };
