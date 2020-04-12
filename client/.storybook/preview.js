4892c109c97f1f99219d11d1ebb34694639e7a97
Carmen Seeba <60884245+Caruki@users.noreply.github.com>
2020-04-12 17:24:45 +0200

    Add storybook viewport addon (#18)
    * Install storybook viewport addon
    
    * Add configuration
    
    * Adjust config to show detailed device options

---
+++ client/.storybook/preview.js
@@ -0,0 +1,9 @@
+import { addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'iphone6',
+  },
+});
