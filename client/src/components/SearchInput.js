2fcb953d8b28cd58a2efbaef268c2bc6a16f7cd2
Carmen Seeba <60884245+Caruki@users.noreply.github.com>
2020-04-14 12:22:32 +0200

    Add various basic input fields with stories (#15)
    * Add styled component and story
    
    * Update client/src/components/FormInput.js
    
    Refactor and optimize styling
    
    Co-Authored-By: marwinburesch <48555130+marwinburesch@users.noreply.github.com>
    
    * Rename file to fit actual function more
    
    * Simplify and optimize styling
    
    * Optimize styling with shorthand prop
    
    * Add username input variation
    
    * Optimize alt names for icons

---
+++ client/src/components/SearchInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import InputField from './InputField';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+  height: 65px;
+  border: 2px solid #961f56;
+  border-radius: 11px;
+  background-color: rgba(30, 25, 79, 0.8);
+`;
+
+const Input = styled(InputField)`
+  width: 80%;
+  font-size: 0.9rem;
+  border: 1px #961f56;
+  border-style: hidden hidden solid hidden;
+`;
+
+function SearchInput() {
+  return (
+    <Container>
+      <Input type="search" placeholder="Search for a tv show..." />
+    </Container>
+  );
+}
+
+export default SearchInput;
---
+++ client/src/stories/InputFields.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SignInUpInput from '../components/SignInUpInput';
+import SearchInput from '../components/SearchInput';
+
+export default {
+  title: 'InputFields',
+};
+
+export const Email = () => {
+  return <SignInUpInput variation="E-Mail" type="email" />;
+};
+
+export const Password = () => {
+  return <SignInUpInput variation="Password" type="password" />;
+};
+
+export const Username = () => {
+  return <SignInUpInput variation="Username" type="text" />;
+};
+
+export const Search = () => <SearchInput />;
