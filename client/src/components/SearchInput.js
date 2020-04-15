6d3a11e81f7ffb1b189318616d326819bc71c68f
Caruki <caruki91@posteo.de>
2020-04-15 23:52:42 +0200

    Add sign up/sign in form variations

--- client/src/components/SearchInput.js
+++ client/src/components/SearchInput.js
@@ -6,7 +6,6 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 60%;
   height: 65px;
   border: 2px solid #961f56;
   border-radius: 11px;
--- client/src/components/SignInUpInput.js
+++ client/src/components/SignInUpInput.js
@@ -7,7 +7,6 @@ import UsernameIcon from '../assets/username.svg';
 import InputField from './InputField';
 
 const Container = styled.div`
-  width: 22%;
   height: 50px;
   display: flex;
   flex-flow: row nowrap;
@@ -20,29 +19,29 @@ const Container = styled.div`
 const InputIconContainer = styled.div`
   width: 25px;
   margin: 0px 10px 0px 13px;
-  padding-top: ${(props) => (props.variation === 'email' ? '5px' : '0px')};
+  padding-top: ${(props) => (props.variation === 'email' ? '20px' : '15px')};
 `;
 
-function SignInUpInput({ variation, type }) {
+function SignInUpInput({ variation, type, placeholder }) {
   return (
     <Container>
-      {variation && variation === 'E-Mail' && (
+      {variation && variation === 'email' && (
         <InputIconContainer variation="email">
           <img src={EmailIcon} alt="email icon showing an email symbol" />
         </InputIconContainer>
       )}
-      {variation && variation === 'Password' && (
+      {variation && variation === 'password' && (
         <InputIconContainer variation="password">
           <img src={PasswordIcon} alt="password icon showing a lock symbol" />
         </InputIconContainer>
       )}
-      {variation && variation === 'Username' && (
+      {variation && variation === 'username' && (
         <InputIconContainer variation="username">
           <img src={UsernameIcon} alt="username icon showing a person symbol" />
         </InputIconContainer>
       )}
 
-      <InputField placeholder={variation} type={type} />
+      <InputField placeholder={placeholder} type={type} />
     </Container>
   );
 }
@@ -50,6 +49,7 @@ function SignInUpInput({ variation, type }) {
 SignInUpInput.propTypes = {
   variation: PropTypes.string,
   type: PropTypes.string,
+  placeholder: PropTypes.string,
 };
 
 export default SignInUpInput;
