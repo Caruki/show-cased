91c8ad86266d3537bd0dc4f1055e67171c31b839
Carmen Seeba <60884245+Caruki@users.noreply.github.com>
2020-04-17 09:27:29 +0200

    Add form for search and submit shows (#25)
    * Add basic form for search and submit shows
    
    * Adjust searchbar input styling for better accessiblity
    
    * Adjust submit button styling for better accessibility
    
    * Adjust sizing of search inputs
    
    * Optimize styling of search form

--- client/src/components/SubmitButton.js
+++ client/src/components/SubmitButton.js
@@ -3,18 +3,19 @@ import styled from '@emotion/styled';
 const SubmitButton = styled.button`
   border: hidden;
   border-radius: 20px;
+  background-color: transparent;
   background-image: linear-gradient(#504481, #1e194f);
   color: #d05888;
+  margin: 10px;
   padding: 13px 45px;
   font: 300 1rem 'Roboto', sans-serif;
+  cursor: pointer;
 
-  & :focus {
+  &:focus {
     outline-width: 0;
-  }
-
-  & :hover {
-    box-shadow: 1px 1px 6px #d05888;
-    text-shadow: 0px 0px 3px #d05888;
+    color: rgba(174, 178, 245, 1);
+    box-shadow: 0px 0px 5px rgba(174, 178, 245, 0.5);
+    border: 1.5px solid rgba(174, 178, 245, 0.5);
   }
 `;
 
