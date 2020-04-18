21d59003be04b4386c31b5e946b0ab64e1775762
Carmen Seeba <60884245+Caruki@users.noreply.github.com>
2020-04-18 15:44:10 +0200

    Outsource watchlist buttons to own component (#35)
    * Outsource watchlistbuttons to own component
    
    * Add accessibility options with focus-within
    
    * Split handleclick events for each button with console.log of future functionality

--- client/src/components/DetailView.js
+++ client/src/components/DetailView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import CloseButton from './CloseButton';
@@ -7,10 +7,7 @@ import TitleSection from './TitleSection';
 import OverviewTextarea from './OverviewTextarea';
 import GenreList from './GenreList';
 import ActorList from './ActorList';
-import WatchlistIcon from '../assets/watchlist-icon.svg';
-import WatchlistIconClicked from '../assets/watchlist-icon-clicked.svg';
-import WatchedIcon from '../assets/watched-icon.svg';
-import WatchedIconClicked from '../assets/watched-icon-clicked.svg';
+import WatchlistButtons from './WatchlistButtons';
 
 const Container = styled.div`
   display: flex;
@@ -26,44 +23,6 @@ const Container = styled.div`
   }
 `;
 
-const WatchlistCheckContainer = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-evenly;
-  padding-bottom: 10px;
-  margin: 5px 10px;
-`;
-
-const WatchListCheckLabel = styled.label`
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  margin: 10px;
-  cursor: pointer;
-
-  & > * {
-    margin: 2.5px 0px;
-  }
-`;
-
-const WatchListCheck = styled.input`
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-  height: 0;
-  width: 0;
-  user-select: none;
-`;
-
-const WatchListCheckText = styled.div`
-  display: block;
-  font-style: ${(props) => (props.checked ? 'italic' : 'normal')};
-  font-size: 0.7rem;
-  font-weight: 200;
-  font-family: 'Roboto', sans-serif;
-  color: #e7eaff;
-  background: transparent;
-`;
 
 function DetailView({
   showPoster,
@@ -73,78 +32,15 @@ function DetailView({
   showGenres,
   showActors,
 }) {
-  const [watchlistAction, setWatchlistAction] = useState(null);
-
-  const addedToWatchlist = watchlistAction === 'addToWatchlist';
-  const addedToWatched = watchlistAction === 'addToWatched';
-
-  function handleWatchlistActionClick(event) {
-    const targetWatchlistAction = event.target.value;
-
-    if (watchlistAction === targetWatchlistAction) {
-      setWatchlistAction(null);
-    } else {
-      setWatchlistAction(targetWatchlistAction);
-    }
-  }
   return (
     <Container>
-   <CloseButton />
+<CloseButton />
     <Poster showPoster={showPoster} />
     <TitleSection showRating={showRating} showTitle={showTitle} />
     <OverviewTextarea showOverview={showOverview} />
     <GenreList genres={showGenres} />
       <ActorList actors={showActors} />
-      <WatchlistCheckContainer>
-        <WatchListCheckLabel>
-          <WatchListCheck
-            type="radio"
-            value="addToWatchlist"
-            checked={addedToWatchlist}
-            onClick={handleWatchlistActionClick}
-          />
-          {addedToWatchlist ? (
-            <img
-              src={WatchlistIconClicked}
-              alt="star icon to symbolize watchlist"
-            />
-          ) : (
-            <img src={WatchlistIcon} alt="star icon to symbolize watchlist" />
-          )}
-          {addedToWatchlist ? (
-            <WatchListCheckText checked={addedToWatchlist}>
-              Added to watchlist
-            </WatchListCheckText>
-          ) : (
-            <WatchListCheckText checked={addedToWatchlist}>
-              Add to watchlist
-            </WatchListCheckText>
-          )}
-        </WatchListCheckLabel>
-
-        <WatchListCheckLabel>
-          <WatchListCheck
-            type="radio"
-            value="addToWatched"
-            checked={addedToWatched}
-            onClick={handleWatchlistActionClick}
-          />
-          {addedToWatched ? (
-            <img
-              src={WatchedIconClicked}
-              alt="check mark icon to symbolize already watched list"
-            />
-          ) : (
-            <img
-              src={WatchedIcon}
-              alt="check mark icon to symbolize already watched list"
-            />
-          )}
-          <WatchListCheckText checked={addedToWatched}>
-            Already watched
-          </WatchListCheckText>
-        </WatchListCheckLabel>
-      </WatchlistCheckContainer>
+      <WatchlistButtons />
     </Container>
   );
 }
---
+++ client/src/components/WatchlistButtons.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import WatchlistIcon from '../assets/watchlist-icon.svg';
+import WatchlistIconClicked from '../assets/watchlist-icon-clicked.svg';
+import WatchedIcon from '../assets/watched-icon.svg';
+import WatchedIconClicked from '../assets/watched-icon-clicked.svg';
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  padding-bottom: 10px;
+  margin: 5px 10px;
+`;
+
+const WatchListCheckLabel = styled.label`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  margin: 10px;
+  padding: 5px;
+  cursor: pointer;
+
+  & :focus-within {
+    outline-width: 0;
+    border: 1px solid #aeb2f5;
+    border-radius: 8px;
+  }
+
+  & > * {
+    margin: 2.5px 0px;
+  }
+`;
+
+const WatchListCheck = styled.input`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+  user-select: none;
+`;
+
+const WatchListCheckText = styled.div`
+  display: block;
+  font-style: ${(props) => (props.checked ? 'italic' : 'normal')};
+  font-size: 0.7rem;
+  font-weight: 200;
+  font-family: 'Roboto', sans-serif;
+  color: #e7eaff;
+  background: transparent;
+`;
+
+function WatchlistButtons() {
+  const [watchlistAction, setWatchlistAction] = useState(null);
+
+  const addedToWatchlist = watchlistAction === 'addToWatchlist';
+  const addedToWatched = watchlistAction === 'addToWatched';
+
+  function handleWatchlistClick(event) {
+    const targetWatchlistAction = event.target.value;
+    console.log('this will do a fetch and move show to towatchlist');
+
+    if (watchlistAction === targetWatchlistAction) {
+      setWatchlistAction(null);
+    } else {
+      setWatchlistAction(targetWatchlistAction);
+    }
+  }
+
+  function handleWatchedClick(event) {
+    const targetWatchlistAction = event.target.value;
+    console.log(
+      'this will do a fetch, move show to watchedlist and if needed remove show from towatchlist'
+    );
+    if (watchlistAction === targetWatchlistAction) {
+      setWatchlistAction(null);
+    } else {
+      setWatchlistAction(targetWatchlistAction);
+    }
+  }
+
+  return (
+    <Container>
+      <WatchListCheckLabel>
+        <WatchListCheck
+          type="radio"
+          value="addToWatchlist"
+          checked={addedToWatchlist}
+          onClick={handleWatchlistClick}
+        />
+        {addedToWatchlist ? (
+          <img
+            src={WatchlistIconClicked}
+            alt="star icon to symbolize watchlist"
+          />
+        ) : (
+          <img src={WatchlistIcon} alt="star icon to symbolize watchlist" />
+        )}
+        {addedToWatchlist ? (
+          <WatchListCheckText checked={addedToWatchlist}>
+            Added to watchlist
+          </WatchListCheckText>
+        ) : (
+          <WatchListCheckText checked={addedToWatchlist}>
+            Add to watchlist
+          </WatchListCheckText>
+        )}
+      </WatchListCheckLabel>
+
+      <WatchListCheckLabel>
+        <WatchListCheck
+          type="radio"
+          value="addToWatched"
+          checked={addedToWatched}
+          onClick={handleWatchedClick}
+        />
+        {addedToWatched ? (
+          <img
+            src={WatchedIconClicked}
+            alt="check mark icon to symbolize already watched list"
+          />
+        ) : (
+          <img
+            src={WatchedIcon}
+            alt="check mark icon to symbolize already watched list"
+          />
+        )}
+        <WatchListCheckText checked={addedToWatched}>
+          Already watched
+        </WatchListCheckText>
+      </WatchListCheckLabel>
+    </Container>
+  );
+}
+
+export default WatchlistButtons;
