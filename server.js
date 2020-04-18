7de54f525c6eeb1465006f660645e8c688d5d6c9
Caruki <caruki91@posteo.de>
2020-04-18 18:50:25 +0200

    Connect to mongoDB and server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+const { connectDB } = require('./lib/database');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -11,6 +12,14 @@ app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running http://localhost:${port}`);
-});
+connectDB()
+  .then(async () => {
+    console.log('Database is live ');
+
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) =>
+    console.log('Oh no something went wrong. Server is down!' + error.status)
+  );
