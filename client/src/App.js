e5f6200ab1983895660b1ddccd96a45c01bc9cfb
Caruki <caruki91@posteo.de>
2020-04-22 14:10:13 +0200

    Add basic routing structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GlobalStyles from './GlobalStyles';
+import styled from '@emotion/styled';
+import Popular from './pages/Popular';
+import Recs from './pages/Recs';
+import Lists from './pages/Lists';
+import Authentication from './pages/Authentication';
+
+// import Header from './components/Header';
+// import BottomNav from './components/BottomNav';
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  background-color: rgba(14, 5, 46, 1);
+  width: 100vw;
+  height: 100vh;
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  flex-grow: 1;
+  overflow: auto;
+  width: 100%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <GlobalStyles />
+        <AppContainer>
+          <MainContainer>
+            <Switch>
+              <Route exact path="/login">
+                <Authentication />
+              </Route>
+              <Route exact path="/register">
+                <Authentication />
+              </Route>
+              <Route exact path="/popular">
+                <Popular />
+              </Route>
+              <Route exact path="/recs">
+                <Recs />
+              </Route>
+              <Route exact path="/lists">
+                <Lists />
+              </Route>
+            </Switch>
+          </MainContainer>
+          {/* <BottomNav /> */}
+        </AppContainer>
+      </Router>
+    </>
   );
 }
 
