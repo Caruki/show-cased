d20cda4d5e04f4208d340c38ff98d5481bfba293
Caruki <caruki91@posteo.de>
2020-04-25 13:00:19 +0200

    Change the redirect router when not authenticated

--- client/src/App.js
+++ client/src/App.js
@@ -48,7 +48,7 @@ function App() {
                 />{' '}
               </>
             ) : (
-              <Redirect to="/" />
+              <Redirect to="/login" />
             )}
             <BasicLayoutRoute path="/" Redirect to="/login" />
           </Switch>
