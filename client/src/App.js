8919658738400dda02436fa6fb37f2a169becd26
Caruki <caruki91@posteo.de>
2020-04-25 21:01:28 +0200

    Change auth routes to user routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import UserProvider from './contexts/user/UserProvider';
 import AuthProvider from './contexts/auth/AuthProvider';
 import GlobalStyles from './GlobalStyles';
 import Authentication from './pages/Authentication';
 import BasicLayoutRoute from './layoutRoutes/BasicLayoutRoute';
 import UserRoutes from './UserRoutes';
-import useAuth from './contexts/auth/useAuth';
 
 function App() {
-  const { isAuthenticated } = useAuth();
-
   return (
     <UserProvider>
       <AuthProvider>
         <Router>
           <GlobalStyles />
           <Switch>
-            <Route exact path="/">
-              <Redirect to="/login" />
-            </Route>
             <BasicLayoutRoute exact path="/login" component={Authentication} />
             <BasicLayoutRoute
               exact
               path="/register"
               component={Authentication}
             />
-            {isAuthenticated ? (
-              <UserRoutes />
-            ) : (
-              <Route path="/">
-                <Redirect to="/login" />
-              </Route>
-            )}
+            <UserRoutes />
           </Switch>
         </Router>
       </AuthProvider>
--- client/src/UserRoutes.js
+++ client/src/UserRoutes.js
@@ -1,26 +1,39 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import Popular from './pages/Popular';
 import Recs from './pages/Recs';
 import Lists from './pages/Lists';
 import HeaderBottomNavLayoutRoute from './layoutRoutes/HeaderBottomNavLayoutRoute';
 import HeaderSideBottomNavLayoutRoute from './layoutRoutes/HeaderSideBottomNavLayoutRoute';
+import useAuth from './contexts/auth/useAuth';
 
 function UserRoutes() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <>
-      <HeaderSideBottomNavLayoutRoute
-        exact
-        path="/popular"
-        component={Popular}
-        site="popular"
-      />
-      <HeaderBottomNavLayoutRoute exact path="/recs" component={Recs} />
-      <HeaderSideBottomNavLayoutRoute
-        exact
-        path="/lists"
-        component={Lists}
-        site="lists"
-      />
+      {isAuthenticated && (
+        <>
+          <HeaderSideBottomNavLayoutRoute
+            exact
+            path="/popular"
+            component={Popular}
+            site="popular"
+          />
+          <HeaderBottomNavLayoutRoute exact path="/recs" component={Recs} />
+          <HeaderSideBottomNavLayoutRoute
+            exact
+            path="/lists"
+            component={Lists}
+            site="lists"
+          />
+        </>
+      )}
+      {!isAuthenticated && (
+        <Route path="/">
+          <Redirect to="/login" />
+        </Route>
+      )}
     </>
   );
 }
