8ccad3c62e14d1da6dbe1e19575d84ccb6d266d5
Caruki <caruki91@posteo.de>
2020-04-29 12:51:57 +0200

    Add watchlist functions

--- client/src/api/lists.js
+++ client/src/api/lists.js
@@ -14,3 +14,54 @@ export async function addToWatchList(userId, show) {
   const confirmation = await response.json();
   return confirmation;
 }
+
+export async function removeFromWatchList(userId, show) {
+  const response = await fetch(`/api/users/${userId}/towatch/remove`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(show),
+  });
+
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+  const confirmation = await response.json();
+  return confirmation;
+}
+
+export async function addToWatchedList(userId, show) {
+  const response = await fetch(`/api/users/${userId}/watched/add`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(show),
+  });
+
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+  const confirmation = await response.json();
+  return confirmation;
+}
+
+export async function removeFromWatchedList(userId, show) {
+  const response = await fetch(`/api/users/${userId}/watched/remove`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(show),
+  });
+
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+  const confirmation = await response.json();
+  return confirmation;
+}
