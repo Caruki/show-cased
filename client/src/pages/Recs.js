2c159a40258b176f263398d713610e018f6846c2
Caruki <caruki91@posteo.de>
2020-05-02 11:24:16 +0200

    Add components to display recommendations

--- client/src/pages/Recs.js
+++ client/src/pages/Recs.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import RecsGenres from '../components/RecsGenres';
+import RecsNetworks from '../components/RecsNetworks';
+import useAuth from '../contexts/auth/useAuth';
 
-function Recs() {
-  return <div>Recs</div>;
+function Lists() {
+  const { authenticatedUser } = useAuth();
+  const userId = authenticatedUser.userId;
+  return (
+    <>
+      <RecsGenres userId={userId} />
+      <RecsNetworks userId={userId} />
+    </>
+  );
 }
 
-export default Recs;
+export default Lists;
