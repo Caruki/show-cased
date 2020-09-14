290d2b081c2f7d28eaa45058fc3f3cb98b15af4e
Caruki <caruki91@posteo.de>
2020-09-14 22:20:01 +0200

    Remove unnecessary config for invalidateQueries

--- client/src/utils/refetchQueries.js
+++ client/src/utils/refetchQueries.js
@@ -1,21 +1,11 @@
 import { queryCache } from 'react-query';
 
 function refetchQueries() {
-  queryCache.invalidateQueries('user', {
-    force: true,
-  });
-  queryCache.invalidateQueries('toWatchList', {
-    force: true,
-  });
-  queryCache.invalidateQueries('watchedList', {
-    force: true,
-  });
-  queryCache.invalidateQueries('recsGenres', {
-    force: true,
-  });
-  queryCache.invalidateQueries('recsNetworks', {
-    force: true,
-  });
+  queryCache.invalidateQueries('user');
+  queryCache.invalidateQueries('toWatchList');
+  queryCache.invalidateQueries('watchedList');
+  queryCache.invalidateQueries('recsGenres');
+  queryCache.invalidateQueries('recsNetworks');
 }
 
 export default refetchQueries;
